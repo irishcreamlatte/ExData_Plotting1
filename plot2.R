de6fe62bace0d8a0a4ab2269395ec3a5ee6d898e
irishcreamlatte <gem.hirang@icloud.com>
2019-08-03 17:51:09 +0800

    plot2 submission

---
+++ plot2.R
@@ -0,0 +1,4 @@
+# Second plot 
+png("plot2.png", width = 480, height = 480)
+with(subset, plot(DateTime, Global_active_power, type = "l", xlab = " ", ylab = "Global Active Power (kilowatts)"))
+dev.off()
